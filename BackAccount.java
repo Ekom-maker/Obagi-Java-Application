3cd611aa3bc2e3bc3f6021b6bd33d94a6109259f
Ekom-maker <uekom7@gmail.com>
2025-04-07 10:56:19 +0100

    Umoh, Ekomobong Chapter 4.docx

---
+++ BackAccount.java
@@ -0,0 +1,25 @@
+public class BackAccount{
+	String accountName = "John";
+	String accountNumber = "4590468934";
+	double accountBalance = "94746.66";
+	
+	public static void main(String[] args){
+		BankAccount bank = new BackAccount();
+		System.out.printf("Account name is %s%n", bank.accountName);
+		System.out.printf("Account number is %s%n", bank.accountNumber);
+		System.out.printf("Account balance is %.2f%n", bank.accountBalance);
+		
+		bank.withdrawal(5000);
+		System.out.printf("Your balance is %.2f.%n", bank.accountBalance);
+	}
+	public double withdrawal(int amount){
+		accountBalance -= amount;
+		
+		return accountBalance;
+	}
+	public double deposit(int amount){
+		accountBalance += amount;
+		
+		return accountBalance;
+	}
+}
